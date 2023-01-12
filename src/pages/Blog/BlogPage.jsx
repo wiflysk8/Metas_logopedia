7978fa8f2f0fd73c334c1b33cf4f67fc8bd1aad8
Luis Martínez <luis.martinez@deus.ai>
2023-01-12 16:22:06 +0100

    add blog

--- src/pages/Blog/BlogPage.jsx
+++ src/pages/Blog/BlogPage.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import kidsImg from "../../assets/images/kids8.JPG";
 import Footer from "../../components/Footer/Footer";
 import { FooterWrapper } from "../../GlobalStyles";
 
 const BlogPage = () => {
   return (
     <section className="mt-28 grid place-items-center ">
-      <h1 className="text-green-200 text-3xl font-bold mb-2">Blog</h1>
-      <div className="w-400 mb-20 relative">
-        <h2 className="absolute">Proximamente...</h2>
-        <img src={kidsImg} alt="group of kids" className="w-full" />
-      </div>
+      {" "}
+      <iframe
+        className="w-full h-screen"
+        src="https://metaslogopedia.blogspot.com"
+        frameborder="0"
+        title="blog"
+      />
       <FooterWrapper>
         <Footer />
       </FooterWrapper>
